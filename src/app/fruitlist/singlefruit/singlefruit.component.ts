ff344fd4589e6e6298480467bfe6b578905a7ced
Oleg Benets <oleg.benets@web.de>
2024-06-30 13:20:20 +0200

    added fruitdata.service and singlefruit components

---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-singlefruit',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './singlefruit.component.html',
+  styleUrl: './singlefruit.component.scss'
+})
+export class SinglefruitComponent {
+  @Input()fruit = {
+    name: "Apfel",
+    img:"apple.png",
+    description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
+    genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
+    stars: 2.3,
+    reviews:[{name: "Waldemar W.", text: "gut für Obstsalat"},{name: "Olaf P.", text: "Kann man mal machen"}],
+  };
+
+  inputData = "";
+
+  @Output()fruitName = new EventEmitter<string>();
+
+  sendInputData() {
+    this.fruitName.emit(this.inputData);
+    this.inputData = "";
+  }
+}
